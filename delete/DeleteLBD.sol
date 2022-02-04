1002772851d37eb66ed0249dd6344934f9475aa0
KeFuCode <fukeee@163.com>
2022-02-04 15:06:29 +0800

    delete

---
+++ delete/DeleteLBD.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^=0.4.0 <0.9.0;
+
+contract DeleteLBC {
+    uint data;
+    uint[] dataArray;
+
+    function f() public {
+        uint x = data;
+        delete x; // 将 x 设为 0，并不影响数据
+        delete data; // 将 data 设为 0，并不影响 x，因为它仍然有个副本
+        uint[] storage y = dataArray;
+        delete dataArray;
+        // 将 dataArray.length 设为 0，但由于 uint[] 是一个复杂的对象，y 也将受到影响，
+        // 因为它是一个存储位置是 storage 的对象的别名。
+        // 另一方面："delete y" 是非法的，引用了 storage 对象的局部变量只能由已有的 storage 对象赋值。
+        assert(y.length == 0);
+    }
+}
